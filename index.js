589b1ad265d055c6d41e884fb9bd35a304e83436
Lorenzo Garcia DallAgnol <lorenzogarciaa@outlook.com>
2020-08-18 09:55:38 -0300

    add tratamento catch connect

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ mongoose.connect('mongo_tj-treinamento:27017/tj-treinamento', {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true
-});
+}).then(success => console.log(success))
+.catch(err => console.log(err))
 app.use(cors());
 app.use(express.json());
 app.use(routes);
