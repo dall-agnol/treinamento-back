e28c7eadaf5586c8a03c1c8b150501dc0c921bc1
Lorenzo DallAgnol <lorenzogarciaa@outlook.com>
2020-10-25 23:49:28 -0300

    changing url mongo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
     useFindAndModify: true,
     useUnifiedTopology: true
 }); */
-mongoose.connect('http://geonosis.mongodb.umbler.com:48738/tj-treinamento', {
+mongoose.connect(process.env.MONGODB_URI + '/tj-treinamento', {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true
